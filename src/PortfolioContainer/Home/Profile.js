22417bf3c8375559ff387f7a7302abecff972639
Duong Van Sua <duongsua.work@gmail.com>
2025-10-03 23:33:54 +0700

    feat(profile): add Profile.js and Profile.css, setup project structure and Bootstrap

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Profile(){
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href="https://web.facebook.com/duong.sua.756671">
+                            <i className="fa fa-facebook-square"></i>
+                        </a>
+                         <a href="#">
+                            <i className="fa fa-google-plus-square"></i>
+                        </a>
+                         <a href="#">
+                            <i className="fa fa-instagram"></i>
+                        </a>
+                         <a href="https://github.com/DuongVanSua">
+                            <i className="fa fa-github"></i>
+                        </a>
+                         <a href="https://www.linkedin.com/in/sua-duong-270444339/">
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
