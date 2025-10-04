e464955db016a30f664ea5314c05a0ccb4dda9ab
Duong Van Sua <duongsua.work@gmail.com>
2025-10-04 16:04:56 +0700

    feat: add profile.css, images, and CV file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+
 import './App.css';
 import Profile from './PortfolioContainer/Home/Profile';
 
--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,29 +1,92 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import './Profile.css';
 
-export default function Profile(){
-    return (
-        <div className="profile-container">
-            <div className="profile-parent">
-                <div className="profile-details">
-                    <div className="colz">
-                        <a href="https://web.facebook.com/duong.sua.756671">
-                            <i className="fa fa-facebook-square"></i>
-                        </a>
-                         <a href="#">
-                            <i className="fa fa-google-plus-square"></i>
-                        </a>
-                         <a href="#">
-                            <i className="fa fa-instagram"></i>
-                        </a>
-                         <a href="https://github.com/DuongVanSua">
-                            <i className="fa fa-github"></i>
-                        </a>
-                         <a href="https://www.linkedin.com/in/sua-duong-270444339/">
-                            <i className="fa fa-linkedin"></i>
-                        </a>
-                    </div>
-                </div>
+
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a
+                href="https://web.facebook.com/duong.sua.756671"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="mailto:yourmail@example.com">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a
+                href="https://instagram.com/yourprofile"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a
+                href="https://github.com/DuongVanSua"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-github"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sua-duong-270444339/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fa fa-linkedin"></i>
+              </a>
             </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Sua</span>
+            </span>
+          </div>
+
+          <div className="profile-details-role">
+            <span className="primary-text">
+              <h1 style={{ fontWeight: 600 }}>
+                <Typewriter
+                  words={[
+                    "Frontend Developer",
+                    "Backend Developer",
+                    "Mobile Developer",
+                    "Full Stack Developer",
+                  ]}
+                  loop={0}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1200}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Proficient in developing applications across frontend and
+                backend operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn"> Hire Me </button>
+            <a href="DuongVanSua_CV.pdf" download="DuongVanSua_CV.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
         </div>
-    )
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
